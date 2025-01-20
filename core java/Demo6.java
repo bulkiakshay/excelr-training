a79db9c2873071c1c5fdf167e2c32b6bf7d80046
bulkiakshay <168890962+bulkiakshay@users.noreply.github.com>
2025-01-20 19:32:59 +0530

    Add files via upload

---
+++ core java/Demo6.java
@@ -0,0 +1,10 @@
+class Demo6{
+    public static void main(String[] args){
+        String usr = "akshay";
+        String psd = "akshay@123";
+        while((usr=="akshay") && (psd=="akshay@123")){
+            System.out.println("hello! welcome to malla reddy university");
+            break;
+        }
+    }
+}
