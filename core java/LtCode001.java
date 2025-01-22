3ccc71c139114edac5e43bd0f283f5c0238f9332
bulkiakshay <168890962+bulkiakshay@users.noreply.github.com>
2025-01-22 11:25:40 +0530

    Leetcode for palindrome number

---
+++ core java/LtCode001.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LtCode001 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int originalNumber = x;
+        int reversedNumber = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            x /= 10;
+        }
+
+        return originalNumber == reversedNumber;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int userInput = scanner.nextInt();
+            LtCode001 solution = new LtCode001();
+                if (solution.isPalindrome(userInput)) {
+            System.out.println(userInput + " is a palindrome.");
+        } else {
+            System.out.println(userInput + " is not a palindrome.");
+        }
+        scanner.close();
+    }
+}
