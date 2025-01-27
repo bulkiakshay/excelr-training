cec9c5950384a228f8ced08694a67a5989033c89
bulkiakshay <168890962+bulkiakshay@users.noreply.github.com>
2025-01-27 10:50:05 +0530

    Names of friends

--- core java/Demo029.java
+++ core java/Demo029.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Demo29 {
+public class Demo029 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String[] friends = new String[10];
